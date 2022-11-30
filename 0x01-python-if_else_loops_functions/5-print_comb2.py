be9f0d4860555ed56e4ba6c4eec9318a86a1f99d
fillzadebayo <temitopeadebayo751@yahoo.com>
2022-11-30 01:23:36 +0000

    6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{0:02d}".format(i), end=", ")
-print("{:d}".format(99))
+for a in range(0, 99):
+    print("{:02d}".format(a), end=", ")
+print("{:d}".format(a + 1))
